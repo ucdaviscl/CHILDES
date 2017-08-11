d0cb8a0ef8009bbb906af4739f5be5797e6e124b
Lauren Namdar <lenamdar@ucdavis.edu>
2017-08-10 21:56:06 -0700

    cleaned up output file

--- gen_conll_freqdist.py
+++ gen_conll_freqdist.py
@@ -12,18 +12,17 @@ infile = sys.argv[1]
 # write words from infile to intermediate file
 # intermediate file will have one word per line
 with open(infile, 'r') as file1, open('conllwords.txt', 'w+') as file2:
-	for line in file1:
-		if(line):
-			line = line.split('\t') # divide CoNLL-formatted file into columns
-			if(len(line) > 1):
-				file2.write('{}\n'.format(line[1])) # get second column of CoNLL file
-	
+    for line in file1:
+        line = line.split('\t') # divide CoNLL-formatted file into columns
+        if(len(line) > 1):
+            file2.write('{}\n'.format(line[1])) # get second column of CoNLL file
+    
 reader = WordListCorpusReader('.', 'conllwords.txt')
 
 words = reader.words() # grab word from each line of text
 fdist = FreqDist(w.lower() for w in words) # generate frequency distribution
 
 # output frequency distribution to file
-with open('conllfdist.txt', 'w') as outfile:
-	for f in fdist:
-		outfile.write('%s \t %d\n' % (f, fdist[f]))
+with open('conll_fdist_words.txt', 'w') as outfile:
+    for f in fdist:
+        outfile.write('%s\n' % (f))
