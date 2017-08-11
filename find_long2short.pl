4f8c952cc5657b3d9be2f780a44a4f3a78736e07
Lauren Namdar <lenamdar@ucdavis.edu>
2017-08-10 22:09:37 -0700

    added hashing

--- find_long2short.pl
+++ find_long2short.pl
@@ -34,81 +34,80 @@ foreach my $fname ( @flist ) {
   my @lengths = ();
   my @output = ();
   my @stack = ();
-  my @cols = ();
+  my %cols = (); # hash of arrays
   my @mins = (); 
 
 
   my $child_long2short_count = 0;
   my $long2short_count = 0; # len1 - len2 > 0
-
+  
     # get length of sentence
     for( my $k = 0; $k < @lines; $k++ ) {
-        @cols = split "\t", $lines[$k];
-        
-    	if( $cols[0] =~ /^\d/ ) {
-    		$min++;
-    	}
-    	
-    		if( $lines[$k] =~ /\-\-\-\-\-/ ) {
-    			push @mins, $min;
-    			$min = 0;
-    		}
-   	}
+        @{$cols{$k}} = split "\t", $lines[$k]; 
 
-  # divide CoNLL formatted text into columns
+        
+        if( $cols{$k}->[0] =~ /^\d/ ) {
+            $min++;
+        }
+        
+            if( $lines[$k] =~ /\-\-\-\-\-/ ) {
+                push @mins, $min;
+                $min = 0;
+            }
+    }
   
   my $idx = 0;
   
-  for( my $i = 0; $i < @lines; $i++ ) {
-    @cols = split "\t", $lines[$i];
-    
+  for( my $i = 0; $i < @lines; $i++ ) {    
     push @output, $lines[$i];
+
+    if ( $cols{$i}->[3] =~ /prep/ ) {
+        push @phrase_idxs, $cols{$i}->[0];
         
-    if ( $cols[3] =~ /prep/ ) {
-    	push @phrase_idxs, $cols[0];
+        if( $cols{$i - 1}->[3] =~ /adv/ ) {
+            push @phrase_idxs, $cols{$i - 1}->[0]; # if immediately preceded by adv, push adv index
+        } 
+        
+        for( my $j = $i + 1; $j < @lines; $j++ ) {              
+            if( $cols{$j}->[7] =~ /POBJ/ ) {
+                push @phrase_idxs, $cols{$j}->[0];  
+                push @stack, $cols{$j}->[0];
+                
+                last; # break
+            }
+            
+             push @phrase_idxs, $cols{$j}->[0];
+        }
+                
+        $min = $mins[$idx];
+        
+         while( @stack ) {
+            $item = pop @stack;
+            
+            if( $item > $max ) {
+                $max = $item;
+            }
+            
+            if( $item < $min ) {
+                $min = $item;
+            }
+            
+            if( @phrase_idxs ) {
+                $dep = pop @phrase_idxs;
+                push @stack, $dep;
+            }
+        }
 
-    	for( my $j = $i + 1; $j < @lines; $j++ ) {
-    	    @cols = split "\t", $lines[$j];
-    	
-    		if( $cols[7] =~ /POBJ/ ) {
-    			push @phrase_idxs, $cols[0];	
-    			push @stack, $cols[0];
-    			
-    			last;
-    		}
-    		 push @phrase_idxs, $cols[0];
+        $length = $max - $min;
 
-		}
-				
-		$min = $mins[$idx];
-		
-		 while( @stack ) {
-    		$item = pop @stack;
-    		
-    		if( $item > $max ) {
-    			$max = $item;
-    		}
-    		
-    		if( $item < $min ) {
-    			$min = $item;
-    		}
-    		
-    		if( @phrase_idxs ) {
-    			$dep = pop @phrase_idxs;
-    			push @stack, $dep;
-    		}
-    	}
-
-    	$length = $max - $min;
-
-    	push @lengths, $length;
-    	
-    	$max = 0;
+        push @lengths, $length;
+        
+        $max = 0;
     }
     
     # if child utterance, set flag variable
     if( index( $lines[$i], "*CHI" ) != -1 ) {
-    	$child_sentence = 1;
+        $child_sentence = 1;
     }
     
     if( $lines[$i] =~ /\-\-\-\-\-/ ) {
@@ -116,24 +115,25 @@ foreach my $fname ( @flist ) {
         $idx++;
 
         if( $difference > 0 ) {
-        
+            print $fh "difference in length: $difference\n";
+            
             if( $child_sentence == 1 ) {
-        	    print $fh "child_long2short_count + 1\n";
-        		$child_long2short_count++;
-        		print $fh "child long2short: $child_long2short_count\n";
-        	}
-        	
-        	else {
-        		print $fh "long2short count + 1\n";
-        		$long2short_count++;
-        		print $fh "long2short: $long2short_count\n";
-        	}
-        	
+                print $fh "child_long2short_count + 1\n";
+                $child_long2short_count++;
+                print $fh "child long2short: $child_long2short_count\n";
+            }
+            
+            else {
+                print $fh "long2short count + 1\n";
+                $long2short_count++;
+                print $fh "long2short: $long2short_count\n";
+            }
+            
             for( my $k = 1; $k < @output; $k++ ) {
-          	  	print $fh "$output[$k]\n";
-          	}
-          	
-          	print $fh "\n";
+                print $fh "$output[$k]\n";
+            }
+            
+            print $fh "\n";
         }
 
         # clear arrays for next utterance
@@ -142,10 +142,8 @@ foreach my $fname ( @flist ) {
         
         # reset flag for next utterance
         $child_sentence = 0;
-
-		#print $fh "\n-----\n";
-		
-    	next;
+        
+        next;
     }
   } 
 }
