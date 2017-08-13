7dc27cb838ace8953646d7ed4d2eabd9675995d7
Lauren Namdar <lenamdar@ucdavis.edu>
2017-08-13 15:20:44 -0700

    added comparison of phrase unigram probabilities

--- inspect_freqs.py
+++ inspect_freqs.py
@@ -9,8 +9,9 @@ infile = sys.argv[1] # file containing CoNLL-formatted text with word frequencie
 
 # dictionary to contain lines of infile
 lines = defaultdict( list )
+phrase_probs = []
 
-phrase_prob = 1 # unigram probability of a prepositional phrase
+phrase_prob = 1 # unigram probability of prepositional phrase
 pp_count = 0 # counter to keep track of prepositional phrases contained in utterance
 i = 0
 
@@ -42,10 +43,10 @@ with open( infile, 'r' ) as file1, open( 'pp_word_freqs.txt', 'a+' ) as file2:
                     phrase_prob = 1
 
                     # if prep immediately preceded by adv
-                    if( len( lines[j - 1] ) > 1 and lines[j - 1][3] == 'adv' ):
+                    if( len( lines[j - 1] ) > 1 and re.match( 'adv', lines[j - 1][3] ) ):
                         phrases[pp_count][lines[j - 1][1]] = lines[j - 1][8]
                         phrase_prob = phrase_prob * float(lines[j - 1][8])
-                        file2.write( lines[j - 1][1] + ': ' + phrases[pp_count][lines[j - 1][1]] + '\n' )
+                        file2.write( lines[j - 1][1] + ':\t' + phrases[pp_count][lines[j - 1][1]] + '\n' )
                     
                     # add word frequencies of each word in pp
                     for k in range( j, len( lines ) ):
@@ -57,10 +58,21 @@ with open( infile, 'r' ) as file1, open( 'pp_word_freqs.txt', 'a+' ) as file2:
                         if( lines[k][7] == 'POBJ' ):
                             phrases[pp_count][lines[k][1]] = lines[k][8]
                             phrase_prob = phrase_prob * float(lines[k][8])
+                            phrase_probs.append(phrase_prob)
                             file2.write( lines[k][1] + ':\t' + phrases[pp_count][lines[k][1]] + '\n\n' )
                             file2.write( 'phrase unigram probability:\t' + str(phrase_prob) + '\n\n' )
                             break # end of pp
 
+        else:
+            if( phrase_probs[0] > phrase_probs[1] ):
+                file2.write( 'pp1 is more probable than pp2 by:\t' + str(phrase_probs[0] - phrase_probs[1]) + '\n\n' )
+            
+            elif( phrase_probs[0] < phrase_probs[1] ):
+                file2.write( 'pp2 is more probable than pp1 by:\t' + str(phrase_probs[1] - phrase_probs[0]) + '\n\n' )
+            
             else:
-                pp_count = 0 # reset for next utterance
-                continue
+                file2.write( 'pp1 and pp2 are equally probable \n\n' )
+                
+            pp_count = 0 # reset for next utterance
+            phrase_probs = []
+            continue
